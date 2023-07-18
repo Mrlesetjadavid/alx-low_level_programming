8de5b5569047842edb1f47d3fadac3fd53d152f0
mrlesetjadavid <mrlesetjadavid@gmail.com>
2023-07-17 19:14:19 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,22 +8,13 @@
 
 int main(void)
 {
-    char lowercase, uppercase;
-    
+    char lowercase, uppercase;    
    
     for(lowercase = 'a'; lowercase <= 'z'; lowercase++) 
-    {
-        putchar(lowercase);
-    }
-    
-    
+            putchar(lowercase);           
     for(uppercase = 'A'; uppercase <= 'Z'; uppercase++) 
-    {
-        putchar(uppercase);
-    }
-    
-    putchar('\n');
-    
+            putchar(uppercase);       
+    putchar('\n');    
     return 0;
 }
 
