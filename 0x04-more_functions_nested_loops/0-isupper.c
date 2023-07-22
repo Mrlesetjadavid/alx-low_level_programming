0bf9e126044a623d2357fc4e906f8bc533f6b2e7
mrlesetjadavid <mrlesetjadavid@gmail.com>
2023-07-21 20:53:24 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - If is uppercase return 1, if not return 0
+ *
+ * Return: 0
+ *
+ **/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
